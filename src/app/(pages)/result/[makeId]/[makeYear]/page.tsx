e874d702036c1ffb9eb0aa1acaa1f33a1172081d
sssgen <deskarte.des@gmail.com>
2024-12-20 18:53:39 +0200

    boosted UX by using suspense

--- src/app/(pages)/result/[makeId]/[makeYear]/page.tsx
+++ src/app/(pages)/result/[makeId]/[makeYear]/page.tsx
@@ -1,29 +1,53 @@
 import VehicleGrid from "@/components/main/VehicleGrid";
 import type { Vehicle } from "@/types/vehicle";
 import { getMakes } from "@/server/vehicles/getMakes";
+import Button from "@/components/Button";
+import Link from "next/link";
+import { Suspense } from "react";
+import Loader from "@/components/Loader";
 
-export default async function Page({
+export default function Page({
     params,
 }: {
     params: { makeId: string; makeYear: string };
 }) {
+    return (
+        <Suspense
+            fallback={
+                <div className='w-screen h-screen flex items-center justify-center'>
+                    <Loader />
+                </div>
+            }
+        >
+            <MainContent params={params} />
+        </Suspense>
+    );
+}
+
+const MainContent = async ({
+    params,
+}: {
+    params: { makeId: string; makeYear: string };
+}) => {
     const { makeId, makeYear } = params;
 
     const vehicles: Vehicle[] = await getMakes(makeId, makeYear);
 
-    switch (true) {
-        case vehicles && vehicles.length > 0:
-            return (
-                <>
-                    <h2 className='text-5xl font-bold font text-white text-center mt-4'>
-                        Found vehicles
-                    </h2>
-                    <VehicleGrid vehicles={vehicles} />
-                </>
-            );
-        case vehicles && vehicles.length === 0:
-            return <p className='text-center'>No vehicles found...</p>;
-        default:
-            return <p className='text-center'>Error fetching vehicles...</p>;
+    if (vehicles && vehicles.length > 0) {
+        return (
+            <>
+                <Link href='/'>
+                    <Button>Back</Button>
+                </Link>
+                <h2 className='text-5xl font-bold font text-white text-center mt-4'>
+                    Found vehicles
+                </h2>
+                <VehicleGrid vehicles={vehicles} />
+            </>
+        );
+    } else if (vehicles && vehicles.length === 0) {
+        return <p className='text-center'>No vehicles found...</p>;
+    } else {
+        return <p className='text-center'>Error fetching vehicles...</p>;
     }
-}
+};
