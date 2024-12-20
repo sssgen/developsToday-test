3cc89395a9cb2ca091475621c3c95383157a220b
sssgen <deskarte.des@gmail.com>
2024-12-20 18:58:28 +0200

    "Back button" inside MakeComponent

--- src/app/(pages)/result/[makeId]/[makeYear]/page.tsx
+++ src/app/(pages)/result/[makeId]/[makeYear]/page.tsx
@@ -35,15 +35,15 @@ const MainContent = async ({
 
     if (vehicles && vehicles.length > 0) {
         return (
-            <>
+            <main className="min-h-screen w-full h-full px-5 overflow-x-hidden">
                 <Link href='/'>
-                    <Button>Back</Button>
+                    <Button className="disabled:opacity-50 disabled:cursor-not-allowed bg-black py-2 px-4 select-none mt-4 ml-4">Back</Button>
                 </Link>
                 <h2 className='text-5xl font-bold font text-white text-center mt-4'>
                     Found vehicles
                 </h2>
                 <VehicleGrid vehicles={vehicles} />
-            </>
+            </main>
         );
     } else if (vehicles && vehicles.length === 0) {
         return <p className='text-center'>No vehicles found...</p>;
